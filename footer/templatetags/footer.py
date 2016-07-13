6d88a70ee0f623d79a058e70babcabaee08d3eb6
Diego <diego@gmail.com>
2016-07-13 15:32:01 +0200

    added menu,header and footer

---
+++ footer/templatetags/footer.py
@@ -0,0 +1,11 @@
+
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('../templates/footer.html', takes_context=True)
+def footer(context):
+    return {
+        'request': context['request'],
+    }
---
+++ header/templatetags/header.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('../templates/header.html', takes_context=True)
+def header(context):
+    return {
+        'request': context['request'],
+    }
